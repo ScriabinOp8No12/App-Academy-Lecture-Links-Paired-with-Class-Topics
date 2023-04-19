63d46654894ce75ada50011f0ab3b67cd311aa7e
Nathan Harwit <nharwit@gmail.com>
2023-04-19 04:39:53 -0600

    [FINAL V1] - Spot checked and everything working as intended. Everything stored in a dictionary with key of the day and values as the topics!

--- scraping-aaOpen-lecture-topics.py
+++ scraping-aaOpen-lecture-topics.py
@@ -2,7 +2,6 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.action_chains import ActionChains
 from bs4 import BeautifulSoup
 import os
 import time
@@ -83,8 +82,7 @@ for week_name in week_names:
         for day_element in day_elements:
             day_text = day_element.text
             if day_text in days:
-                # just click on the first day we see that's valid, that will allow the html to load, and we can
-                # get all the data we need after that
+                # just click on the first day we see that's valid, that will allow the entire html to load
                 browser.execute_script("arguments[0].click();", day_element)
                 print(f"Clicked on {day_text}")
                 # IMPORTANT: Need to wait at least 2 seconds otherwise THE URL WON'T CHANGE (it'll grab wrong data)
@@ -95,40 +93,60 @@ for week_name in week_names:
         current_url = browser.current_url
         print('this is the current url', current_url)
 
-        not_valid_topics = ['Learning Boost', 'End of Day', 'Formative Quiz', 'Practice Problems']
-
         new_result = browser.page_source
         # Update the doc object, otherwise if we use the original doc object, our data will be blank
         each_day_doc = BeautifulSoup(new_result, "html.parser")
 
-        # ** Fixing everything from here on down! **
-
         # **** Light theme and Dark theme have different CLASSES!!!! Selenium opens the browser in light theme, not the dark theme I use normally
-        # use both in the search: h3_elements = each_day_doc.find_all('h3', class_=['sc-dnqmqq ftHzVO', 'sc-dnqmqq kcAQXs'])
+        # if using both in the search: h3_elements = each_day_doc.find_all('h3', class_=['sc-dnqmqq ftHzVO', 'sc-dnqmqq kcAQXs'])
 
-        # all h3 elements on the page
+        # My original code block below to print everything out
+        # h3_elements = each_day_doc.find_all('h3', class_=['sc-dnqmqq kcAQXs', 'sc-dnqmqq jLQnpZ'])
+        # for h3_element in h3_elements:
+        #     print('days:', h3_element.text)
+        # # Get the topic elements from the updated doc object -> these are TOPICS
+        # topic_elements = each_day_doc.find_all('header', class_='sc-gqjmRU kvshPl')
+        # for topic_element in topic_elements:
+        #     # valid topics
+        #     if topic_element.text not in not_valid_topics:
+        #         print("topics:", topic_element.text)
+        # this code block above prints out all the days and topics
+
+        # print(each_day_doc)
+
         h3_elements = each_day_doc.find_all('h3', class_=['sc-dnqmqq kcAQXs', 'sc-dnqmqq jLQnpZ'])
-        for h3_element in h3_elements:
-            print('days:', h3_element.text)
-        # Get the topic elements from the updated doc object
-        topic_elements = each_day_doc.find_all('header', class_='sc-gqjmRU kvshPl')
-        for topic_element in topic_elements:
-            if topic_element.text not in not_valid_topics:
-                print("topics:", topic_element.text)
 
-    #             if h3_element.text in days:
-        #                 #dark theme: sc-gqjmRU homZFz
-        #                 print(h3_element.text)
-        #                 parent_element = h3_element.find_element_by_xpath('..')
-        #                 header_elements = parent_element.find_elements_by_css_selector('header.sc-gqjmRU.kvshPl')
-        #                 for header_element in header_elements:
-        #                     if header_element is not None:
-        #                         topics.append(header_element.text)
-        # print(topics)
+        days = {}
+        valid_days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Saturday']
+        not_valid_topics = ['Learning Boost', 'End of Day', 'Formative Quiz', 'Practice Problems']
+        for h3_element in h3_elements:
+            if h3_element.text in valid_days:
+                current_day = h3_element.text
+                days[current_day] = []
+                next_ul = h3_element.find_next('ul')
+                if next_ul:
+                    topic_lis = next_ul.find_all('li')
+                    for topic_li in topic_lis:
+                        topic_header = topic_li.find('header', class_='sc-gqjmRU kvshPl')
+                        if topic_header and topic_header.text not in not_valid_topics:
+                            days[current_day].append(topic_header.text)
+
+        print(days)
+
 
     browser.get(main_page_url)
     # Have to find the menu_element here again (won't work if we use just the line below that)
     menu_element = browser.find_element(by=By.CSS_SELECTOR, value='a.sc-hwwEjo.ieBOLv')
     browser.execute_script('arguments[0].click();', menu_element)
 
+# **** PLAN!!!!! ****
 
+# Start at the upper most h3 element. Start a loop to go through all the h3 elements.
+# If the text value of the h3 element is not Monday, Tuesday, Wednesday, Thursday, or Saturday, then break.
+# If it is one of those days, then store all of the header elements with class 'sc-gqjmRU kvshPl' text values
+# in a dictionary as values (use a list for the values) with the h3 element as the key.
+# for the starting one, that would be Monday as the key, with values of the heading element with class
+# elements with class 'sc-gqjmRU kvshPl' which is {'Monday', ['Orientation', Skills Survey']
+# now since it finished finding all those heading elements, it starts over at the next h3 element in the loop.
+#
+#
