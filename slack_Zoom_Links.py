549ca0997ded6674b1ab1ead1cd59f1a8bc810a0
Nathan Harwit <nharwit@gmail.com>
2023-04-19 23:58:41 -0600

    renamed zoom links python file

---
+++ slack_Zoom_Links.py
@@ -0,0 +1,7 @@
+# use a legacy token
+# grab zoom links with associate dates
+# Check time of posting, and then figure out which date that must go with
+# convert that date into the format of week + Monday/Tuesday/Wednesday/Thursday/Saturday to line up with
+# AA open topics.  Store these in a dictionary
+
+# Use Google Sheets API to store this output there ->
