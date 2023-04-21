947df873632a1bfe96dd47ef9205effa08531d8f
Nathan Harwit <nharwit@gmail.com>
2023-04-20 19:16:55 -0600

    added slack zoom links boiler plate code that doesn't work yet -> need to figure out how to tell selenium to load my normal chrome session properly instead of loading it's own and forcing me to log in (it says I can't because the browser is insecure)

--- slack_Zoom_Links.py
+++ slack_Zoom_Links.py
@@ -5,3 +5,44 @@
 # AA open topics.  Store these in a dictionary
 
 # Use Google Sheets API to store this output there ->
+# *** I'm just going to scrape Slack ***
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from bs4 import BeautifulSoup
+import os
+import time
+import requests
+import pandas as pd
+
+
+from selenium import webdriver
+
+options = webdriver.ChromeOptions()
+options.add_argument("--user-data-dir=C:\\Users\\nharw\\AppData\\Local\\Google\\Chrome\\User Data") # replace with the path to your Chrome profile
+browser = webdriver.Chrome(options=options)
+
+slack_channel_link = 'https://app.slack.com/client/T03GU501J/C04EKPNLTQ9/thread/C03GUB0T3-1673626274.605469'
+browser.get(slack_channel_link)
+
+#
+# options = webdriver.ChromeOptions()
+# options.add_argument(r'user-data-dir=C:\Users\nharw\AppData\Local\Google\Chrome\User Data') #Path to my chrome profile
+# options.add_argument('--no-sandbox')
+# options.add_argument('--disable-dev-shm-usage')
+# service = Service("C:\\Users\\chromedriver.exe")
+# browser = webdriver.Chrome(service=service, options=options)
+#
+# slack_channel_link = 'https://app.slack.com/client/T03GU501J/C04EKPNLTQ9/thread/C03GUB0T3-1673626274.605469'
+# browser.get(slack_channel_link)
+
+
+
+
+
+
+
