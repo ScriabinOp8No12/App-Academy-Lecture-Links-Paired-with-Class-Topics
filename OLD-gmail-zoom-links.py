3b91d800246c8df241d433192114dfbc5b4cc079
Nathan Harwit <nharwit@gmail.com>
2023-04-29 21:26:18 -0600

    added plan for how to extract old zoom links from April 20th email chain

---
+++ OLD-gmail-zoom-links.py
@@ -0,0 +1,3 @@
+# This python file will extract the zoom links from the older recordings (before April 20th)
+# They are all within one giant email (from April 20th, 11:40pm MT), so it shouldn't be too hard.
+# All the recent / new emails arrive in my inbox once the lecture is recorded
