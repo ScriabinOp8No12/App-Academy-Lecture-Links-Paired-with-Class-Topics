99815d27eda674bc3d919da98b05ab66af76a60f
Nathan Harwit <nharwit@gmail.com>
2023-05-03 11:33:04 -0600

    added comments and got dates to be in reverse chronological order by specifying the last row that contained data in it, and adding to the last row each time

--- gsheets_conversion.py
+++ gsheets_conversion.py
@@ -17,12 +17,14 @@ client = gspread.authorize(creds)
 # Open the Google Sheet (specify the sheet name)
 sheet = client.open_by_key(SPREADSHEET_KEY).worksheet('Jan-9th-Cohort-Lectures')
 
-# Get the email data from the first code
+# Get the email data from the gmail_api email output ('NEW_gmail_zoom_links.py')
 dates_zoom_links_and_topics = emails_data
 
-# Dynamically add these values now -> read these values from the "NEW_gmail_zoom_links.py" file
-# and load them into the Google sheets
+# Look at values in 1st column of Google sheets so that we can check if that date already exists, if it does
+# then do NOT update the Google sheet with that data (it'll be a duplicate)
+dates_in_sheet = sheet.col_values(1)
 
+# Dynamically add the values from the "NEW_gmail_zoom_links.py" file and add them into the Google sheets
 for email_data in emails_data:
     # Original date string: Date: May 2, 2023 03:58 PM Central Time (US and Canada)
     date_str = email_data['date']
@@ -36,10 +38,17 @@ for email_data in emails_data:
     formatted_date = date_obj.strftime('%B %d, %Y')
     zoom_link = email_data['zoom_link']
     passcode = email_data['passcode']
-    # topics_from_aa = email_data['topics']
-    # values = [date, zoom_link, passcode, topics_from_aa]
-    values = [formatted_date, zoom_link, passcode]
-
-    # Insert a new row at the 2nd column of the sheet and input the values there!
-    sheet.insert_row(values, 2)
+    # If the Date doesn't show up in the Google sheet date column, only then do we update the values
+    if formatted_date not in dates_in_sheet:
+        # NEXT STEP (add before if statement): Add App Academy Open topics to end of values list
+        values = [formatted_date, zoom_link, passcode]
+        # The next 2 lines of code adds the dates in chronological order (oldest at the top)
+        # # Insert a new row at the 2nd column of the sheet and input the values there!
+        # sheet.insert_row(values, 2)
+        # ----------------------------------------------------------
+        # The following block of code adds the dates in reverse chronological order (newest lectures at the top)
+        # Find the last row of the sheet which has data in it (if it's empty, we start on row 1)
+        last_row = len(sheet.col_values(1))
+        # Insert a new row after the last row of the sheet and input the values there!
+        sheet.insert_row(values, last_row + 1)
 
