571141204dae11b3485d9f8d6b35e31f9b8ec74d
Nathan Harwit <nharwit@gmail.com>
2023-05-11 11:35:13 -0600

    [WORKING non duplicate dates!] -> used a set to keep track of the dates in the first column, then if the date isn't in the set, add that date into the set and continue the logic, it works now!

--- gsheets_conversion.py
+++ gsheets_conversion.py
@@ -15,14 +15,24 @@ creds = ServiceAccountCredentials.from_json_keyfile_name('gmail-connector-gsheet
 client = gspread.authorize(creds)
 
 # Open the Google Sheet (specify the sheet name)
-sheet = client.open_by_key(SPREADSHEET_KEY).worksheet('Jan-9th-Cohort-Lectures')
+# sheet = client.open_by_key(SPREADSHEET_KEY).worksheet('Jan-9th-Cohort-Lectures')
+sheet = client.open_by_key(SPREADSHEET_KEY).worksheet('Testing_Sheet')
 
 # Get the email data from the gmail_api email output ('NEW_gmail_zoom_links.py')
 dates_zoom_links_and_topics = emails_data
 
 # Look at values in 1st column of Google sheets so that we can check if that date already exists, if it does
-# then do NOT update the Google sheet with that data (it'll be a duplicate)
-dates_in_sheet = sheet.col_values(1)
+# then do NOT update the Google sheet with that data (it'll be a duplicate) AND also add it to a set so we
+# avoid lectures that are not valid (see below)
+# Gmail api extracts the lectures with the most recent time (or latest time) first, so it's important to NOT run
+# this program until after all the Gmail zoom links have arrived in your Gmail inbox.  For example, let's say
+# at 5pm MT and 6pm MT, a lecture link is sent to your gmail. In our cohort, these are mistakes, and are 1-2 minute
+# long lectures with nothing on them.  At 11:30pm MT, a third and final email comes in with a link, since the
+# Pacific Time cohort ends at 11pm MT, we know that this is the correct Zoom info to add to our Google sheets!
+# The Gmail API extracts them in the most recent order, so we can simply run this program, extract the correct
+# 11:30pm MT lecture, then store it in a set. Now if we try to grab the other ones from the same date, it won't
+# add those to the Google Sheets.
+dates_in_sheet = set(sheet.col_values(1)[1:])
 
 # Dynamically add the values from the "NEW_gmail_zoom_links.py" file and add them into the Google sheets
 for email_data in emails_data:
@@ -40,6 +50,8 @@ for email_data in emails_data:
     passcode = email_data['passcode']
     # If the Date doesn't show up in the Google sheet date column, only then do we update the values
     if formatted_date not in dates_in_sheet:
+        # Add that date into the set, so we don't get duplicates
+        dates_in_sheet.add(formatted_date)
         # NEXT STEP (add before if statement): Add App Academy Open topics to end of values list
         values = [formatted_date, zoom_link, passcode]
         # Find the last row of the sheet which has data in it (if it's empty, we start on row 1)
@@ -60,6 +72,7 @@ for email_data in emails_data:
     # Update the sheet with the sorted data (keeping the header row in place)
     sheet.update('A2', data)
 
+# print(dates_in_sheet)
 # Even after sorting, the same dates stay in the same position, so we can run one more filter at the very end that removes
 # any of the rows after the first time that date shows up. For example, if there are 3 May 5ths, find the row with the lower row number
 # and then delete all the other rows
