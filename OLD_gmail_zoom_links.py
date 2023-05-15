1e3b682de8c7d78cfda57ffa3d892474b677a073
Nathan Harwit <nharwit@gmail.com>
2023-05-15 11:13:12 -0600

    got the body of the gmail attachments, but it's extremely challenging to extract the proper information because the output is really strange.  It should be identical to the current email format, but I've tried for a few hours and still can't extract the proper zoom links

--- OLD_gmail_zoom_links.py
+++ OLD_gmail_zoom_links.py
@@ -1,90 +1,73 @@
 # This python file will extract the zoom links from the older recordings (before April 20th)
 # They are all within one giant email (from April 20th, 11:40pm MT), so it shouldn't be too hard.
-# All the recent / new emails arrive in my inbox once the lecture is recorded
+# All the recent / new emails that came in after April 20th arrive in my inbox once the lecture is recorded
+# and are from a different sender
 
-from google.oauth2.credentials import Credentials
+# Need to specify a specific email with all 50 old lecture recordings since I received multiple
+# emails from my instructor, and I don't want to have to filter anything
 from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
-from bs4 import BeautifulSoup
-from datetime import datetime
-import re
+from google.oauth2.credentials import Credentials
 import base64
+from bs4 import BeautifulSoup
+import os
+import re
+from googleapiclient.discovery import build
+from google.oauth2.credentials import Credentials
+import base64
+from bs4 import BeautifulSoup
 import os
 
-emails_data = []
-def get_emails_from_sender(access_token, refresh_token, client_id, client_secret, sender):
-    try:
-        # Create credentials object from access token
-        creds = Credentials.from_authorized_user_info(info={
-            'access_token': access_token,
-            'refresh_token': refresh_token,
-            'client_id': client_id,
-            'client_secret': client_secret
-        })
-        # Create Gmail API service
-        service = build('gmail', 'v1', credentials=creds)
-        # Get the user's email address
-        user_id = service.users().getProfile(userId='me').execute()['emailAddress']
-        # Create query to search for emails from specific sender
-        query = f'from:{sender}'
-        # Get list of messages matching the query
-        response = service.users().messages().list(userId=user_id, q=query).execute()
-        messages = response.get('messages', [])
-        # Get the content of each message
-        for message in messages:
-            msg = service.users().messages().get(userId=user_id, id=message['id']).execute()
-            payload = msg['payload']
-            if 'parts' in payload:
-                parts = payload['parts']
-                data = parts[0]['body']['data']
-            else:
-                data = payload['body']['data']
+def get_attachments_from_email(access_token, refresh_token, client_id, client_secret, sender, email_subject):
+    # Create credentials object from access token
+    creds = Credentials.from_authorized_user_info(info={
+        'access_token': access_token,
+        'refresh_token': refresh_token,
+        'client_id': client_id,
+        'client_secret': client_secret
+    })
+    # Create Gmail API service
+    service = build('gmail', 'v1', credentials=creds)
+    # Get the user's email address
+    user_id = service.users().getProfile(userId='me').execute()['emailAddress']
+    # Create query to search for emails from specific sender with specific subject
+    query = f'from:{sender} subject:{email_subject}'
+    # Get list of messages matching the query
+    response = service.users().messages().list(userId=user_id, q=query).execute()
+    messages = response.get('messages', [])
+    # Get the first message (assuming there is only one message with the specified sender and subject)
+    message = messages[0]
+    msg = service.users().messages().get(userId=user_id, id=message['id']).execute()
+    payload = msg['payload']
+    attachments_data = []
+    # Iterate over the parts of the message payload to find attachments
+    for part in payload['parts']:
+        if part['filename']:
+            attachment_id = part['body']['attachmentId']
+            attachment = service.users().messages().attachments().get(userId=user_id, messageId=message['id'], id=attachment_id).execute()
+            data = attachment['data']
             # Decode base64 data
             data = data.replace('-', '+').replace('_', '/')
-            decoded_data = base64.b64decode(data)
-            # Convert decoded data to string
-            content = decoded_data.decode('utf-8')
-            # Parse email content using BeautifulSoup
-            soup = BeautifulSoup(content, 'html.parser')
-            # Find passcode
-            passcode = soup.find('span').text.split(': ')[1]
+            decoded_data = base64.urlsafe_b64decode(data)
+            attachments_data.append(decoded_data)
 
-            # Find zoom link -> logic, find the div with the text "you can copy the recording info..."
-            # then find it's next sibling, which is always the proper zoom link. Look for an 'a' tag and href attribute
-            zoom_link = soup.find('div',
-                                  string='You can copy the recording information below and share with others').find_next_sibling(
-                'a')['href']
-            # Find date information using regex
-            # [FOR LATER!] if multiple recordings with same date, only use the one that is the most recent (latest time)
-            date = re.search(r'Date: .+', content)
-            if date:
-                date = date.group()
-                # print(f'Date: {date}')
-            email_data = {
-                'date': date,
-                'zoom_link': zoom_link,
-                'passcode': passcode
-            }
-            emails_data.append(email_data)
+    attachments_html = []
+    for data in attachments_data:
+        content = data.decode('utf-8')
+        soup = BeautifulSoup(content, 'html.parser')
+        body_html = str(soup.body)
 
+        attachments_html.append(body_html)
 
-            # # print(content)
-            # print(f'Zoom link: {zoom_link}')
-            # print(f'Passcode: {passcode}')
-
-    except HttpError as error:
-        print(f'An error occurred: {error}')
-
-    #print(emails_data)
-    return emails_data
+    return attachments_html
 
 access_token = os.environ['ACCESS_TOKEN']
 refresh_token = os.environ['REFRESH_TOKEN']
 client_id = os.environ['CLIENT_ID']
 client_secret = os.environ['CLIENT_SECRET']
 sender = os.environ['INSTRUCTOR_EMAIL']
+email_subject = 'recordings'
 
+attachments_html = get_attachments_from_email(access_token, refresh_token, client_id, client_secret, sender, email_subject)
 
-get_emails_from_sender(access_token, refresh_token, client_id, client_secret, sender)
-
-print(sender)
+# Print out attachments HTML
+print(attachments_html)
