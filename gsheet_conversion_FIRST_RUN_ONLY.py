3f37059902518ae0d0fd461d0d8b918c5184d5d1
Nathan Harwit <nharwit@gmail.com>
2023-06-23 14:33:50 -0600

    Properly runs on first run, took 6 minutes for 2 months of data to successfully populate the entire sheet, the time.sleep(3) could probably be time.sleep(2) and it would still work, but it's not worth the hassle to risk it meeting the max quota, it was 216 requests so no where near the 60 requests per minute limit.

---
+++ gsheet_conversion_FIRST_RUN_ONLY.py
@@ -0,0 +1,126 @@
+import gspread
+import os
+from oauth2client.service_account import ServiceAccountCredentials
+from NEW_gmail_zoom_links import emails_data
+# Can speed up this import by writing the data to a text file once, then just reading from it
+from converting_scraped_topics_to_have_date_in_key import new_list_of_dictionaries
+from datetime import datetime
+import time
+
+startTime = time.time()
+# Spreadsheet key is found on the Google sheets url, between the /d/ and /edit
+SPREADSHEET_KEY = os.environ['SPREADSHEET_KEY']
+# Authenticate using a service account
+scope = ['https://spreadsheets.google.com/feeds',
+         'https://www.googleapis.com/auth/drive']
+# Replace first parameter with the json file that's in this pycharm project
+creds = ServiceAccountCredentials.from_json_keyfile_name('gmail-connector-gsheets.json', scope)
+client = gspread.authorize(creds)
+# Open the Google Sheet (specify the sheet name you want to populate)
+# sheet = client.open_by_key(SPREADSHEET_KEY).worksheet('Jan-9th-Cohort-Lectures')
+# sheet = client.open_by_key(SPREADSHEET_KEY).worksheet('Testing_Sheet')
+sheet = client.open_by_key(SPREADSHEET_KEY).worksheet('Testing_Time_Complexity_1')
+
+data = sheet.get_all_values()
+# Separate the header row from the rest of the data
+header = data[0]
+data = data[1:]
+# Update the sheet with the sorted data (keeping the header row in place)
+sheet.update('A2', data)
+# Get the email data from the gmail_api email output ('NEW_gmail_zoom_links.py')
+dates_zoom_links_and_topics = emails_data
+
+# See 1. for logic
+dates_in_sheet = set(sheet.col_values(2)[1:])
+counter = 0
+# Dynamically add the values from the "NEW_gmail_zoom_links.py" file and add them into the Google sheets
+for email_data in emails_data:
+    counter += 1
+    print(counter)
+
+    # Original date string: Date: May 2, 2023 03:58 PM Central Time (US and Canada)
+    date_str = email_data['date']
+    # Split between the end of "Date:" and before "Central Time"
+    # Output is now: May 2, 2023 03:58 PM
+    date_str = date_str.split('Date: ')[1].split('Central Time')[0].strip()
+    # Parse string into a datetime object, based on the following format
+    # %b: month name, %d: zero padded day, %Y: 4 digit year, etc
+    # Parse string into a datetime object, based on the following format
+    # %b: month name, %d: zero padded day, %Y: 4 digit year, etc
+    date_obj = datetime.strptime(date_str, '%b %d, %Y %I:%M %p')
+    # We only want the month, day, and year.  Output is: May 02, 2023
+    # Could change formatting here to remove the ' in front of the dates in Excel to the line below!
+    # formatted_date = date_obj.strftime('%Y-%m-%d')
+    formatted_date = date_obj.strftime('%B %d, %Y')
+    zoom_link = email_data['zoom_link']
+    passcode = email_data['passcode']
+    # If the Date doesn't show up in the Google sheet date column, and the date isn't a Friday or Sunday
+    if formatted_date not in dates_in_sheet and date_obj.weekday() not in [4, 6]:
+        # Add that date into the set, so we don't get duplicates
+        dates_in_sheet.add(formatted_date)
+        # Get the day of the week as a string
+        day_of_week = date_obj.strftime('%A')
+        values = [day_of_week, formatted_date, zoom_link, passcode]
+        # Find the last row of the sheet which has data in it (if it's empty, we start on row 1)
+        last_row = len(sheet.col_values(1))
+        # Insert a new row after the last row of the sheet and input the values there!
+        sheet.insert_row(values, last_row + 1)
+    # ***** SORT the data in descending order, but don't also sort the 1st row because it has the headers! *****
+    # Get all data from the sheet (including the header row)
+    data = sheet.get_all_values()
+    # Separate the header row from the rest of the data
+    header = data[0]
+    data = data[1:]
+    # Sort the data by the date column (column index 1) in descending order
+    date_column_index = 1
+    data = sorted(data, key=lambda x: datetime.strptime(x[date_column_index], '%B %d, %Y'), reverse=True)
+    # Update the sheet with the sorted data (keeping the header row in place)
+    sheet.update('A2', data)
+    # Pause here for 3 seconds to not go over the max quota requests
+    time.sleep(3)
+
+# Adding AA Topics to Google Sheets based on date:
+for week in new_list_of_dictionaries:
+    counter += 1
+    print(counter)
+    for date, topics in week.items():
+        counter += 1
+        print(counter)
+        # Check if the date is in the Google Sheets
+        if date in dates_in_sheet:
+            # Finds the row that contains the date by using the find method of the sheet object and passing in the date
+            # as an argument. The find method returns a Cell object that has a row attribute representing the row number of the cell.
+            # This row number is then assigned to the row_index variable.
+            row_index = sheet.find(date).row
+            # Update the topics column (column 5) with the topics
+            # The update_cell method of the sheet object is used to update the cell in column 5 of the row specified
+            # The last parameter of the update_cell method joins all the elements in the
+            # topics list into a single string separated by , .
+            # For example, if topics = ['Topic 1', 'Topic 2', 'Topic 3'], then ', '.join(topics)
+            # would return 'Topic 1, Topic 2, Topic 3'
+            sheet.update_cell(row_index, 5, ', '.join(topics))
+            counter += 1
+            print(counter)
+            # Sleep for 3 seconds, to not reach the maximum quota and have program break
+            time.sleep(3)
+
+executionTime = (time.time() - startTime)
+print('Execution time in seconds: ' + str(executionTime))
+
+# You might run into a "temporary error" with some html output, if that happens, just run the code again!
+# On the first time you run the code, you will want to change everywhere you see the commented out
+# time.sleep(1) to time.sleep(3) to not exceed the max quota!  However, on subsequent runs when you are only adding
+# 1 row at a time, you can omit the time.sleep()
+
+# 1.
+# Look at values in 1st column of Google sheets so that we can check if that date already exists, if it does
+# then do NOT update the Google sheet with that data (it'll be a duplicate) AND also add it to a set so we
+# avoid lectures that are not valid.
+# Gmail api extracts the lectures with the most recent time (or latest time) first, so it's important to NOT run
+# this program until after all the Gmail zoom links have arrived in your Gmail inbox.  For example, let's say
+# at 5pm MT and 6pm MT, a lecture link is sent to your gmail. In our cohort, these are mistakes, and are 1-2 minute
+# long lectures with nothing on them.  At 11:30pm MT, a third and final email comes in with a link, since the
+# Pacific Time cohort ends at 11pm MT, we know that this is the correct Zoom info to add to our Google sheets!
+# The Gmail API extracts them in the most recent order, so we can simply run this program, extract the correct
+# 11:30pm MT lecture, then store it in a set. Now if we try to grab the other ones from the same date, it won't
+# add those to the Google Sheets.
